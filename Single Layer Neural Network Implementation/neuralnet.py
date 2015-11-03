773ebeb406f843b966bc4d8634f2800e7324b3f9
Satyam Dhar <sdhar@cs.wisc.edu>
2015-11-03 00:59:07 -0600

    Removed commented code

--- Single Layer Neural Network Implementation/neuralnet.py
+++ Single Layer Neural Network Implementation/neuralnet.py
@@ -30,7 +30,7 @@ class trainInst:
         self.value=self.attrValues[AttributeIndex]
         
     def display(self):
-        print self.attrValues
+         self.attrValues
 
 
 class Attribute:
@@ -47,16 +47,13 @@ def divideByClass(trainingInstances):
         else :
             class1instances.append(instance)
     return (class0instances,class1instances)
-# def updateWeights(w0,weights,setInstances):
 
 def makeSubsets(class0instances, class1instances, numOfNegativesInEach, numOfPositivesInEach):
     subset=[]
     listOfSubsets=[]
     lenClass0 = len(class0instances)
     lenClass1 = len(class1instances)
-    #numOfFolds = 0
     while len(listOfSubsets)+1<=n and lenClass0 >= numOfNegativesInEach and lenClass1 >= numOfPositivesInEach :
-#         print "lengths", lenClass0, lenClass1
         elementsToRemoveIn0=[]
         elementsToRemoveIn1=[]
         subsetIndexList = random.sample(range(0,lenClass0),numOfNegativesInEach)
@@ -64,37 +61,27 @@ def makeSubsets(class0instances, class1instances, numOfNegativesInEach, numOfPos
             subset.append(class0instances[i])
             class0instances[i].fold=len(listOfSubsets)+1
             elementsToRemoveIn0.append(class0instances[i])
-#             print i,
-#             class0instances.remove(class0instances[i])
         subsetIndexList = random.sample(range(0,lenClass1),numOfPositivesInEach)
         for i in subsetIndexList :
             subset.append(class1instances[i])
             class1instances[i].fold=len(listOfSubsets)+1
             elementsToRemoveIn1.append(class1instances[i])
-#             class1instances.remove(class1instances[i])
-#         elementsToRemoveIn0.sort(key=None, reverse=True)
-#         elementsToRemoveIn1.sort(key=None, reverse=True)
-        
-#         print "fold is",elementsToRemoveIn0[0].fold
+
         for instance in elementsToRemoveIn0:
             class0instances.remove(instance)
         for instance in elementsToRemoveIn1:
             class1instances.remove(instance)
         lenClass0 = len(class0instances)
         lenClass1 = len(class1instances)
-#         print "lengths", lenClass0, lenClass1
 
         listOfSubsets.append(subset)
-        #numOfFolds+=1
         subset=[]
     if lenClass0 > 0 :
         for i in range(len(class0instances)):
-#             print listOfSubsets[i]
             listOfSubsets[i].append(class0instances[i])
             class0instances[i].fold=i+1
     if lenClass1 > 0 :
         for i in range(len(class1instances)) :
-#             print i
             listOfSubsets[len(listOfSubsets)-1-i].append(class1instances[i])
             class1instances[i].fold=len(listOfSubsets)-i
     return listOfSubsets
@@ -130,10 +117,8 @@ def main():
         attributeObj = Attribute(attribute,attributeIndex)
         attributes.append(attributeObj)
     
-#     Y = []
 
     for instance in trainingInstances:
-#         if isinstance(instance.attrValues[-1], String):
         if instance.attrValues[-1] == 'Mine':
             instance.attrValues[-1] = 1
         else :
@@ -147,29 +132,17 @@ def main():
     
     
     (class0instances,class1instances) = divideByClass(trainingInstances)
-#     print len(class0instances),len(class1instances) 
     numOfinstancesInSampleSubsetWithClass0 = int((len(class0instances)/n))
     numOfinstancesInSampleSubsetWithClass1 = int((len(class1instances)/n))
-    #     numOfinstancesInSampleSubsetWithClass0 = numOfinstancesInSampleSubsetWithClass1 = (len(trainingInstances)/n)/2
     
-#     print "numbers",numOfinstancesInSampleSubsetWithClass0,numOfinstancesInSampleSubsetWithClass1
     listOfSubsets=makeSubsets(class0instances,class1instances,numOfinstancesInSampleSubsetWithClass0,numOfinstancesInSampleSubsetWithClass1)
-#     for instance in class0instances:
-#         print "fold is",instance.fold
-    
-#     for subset in listOfSubsets:
-#         print len(subset),
     for k in range(e):
         for curr in range(len(listOfSubsets)):
-#             correctOutputs = 0
             for i in range(len(listOfSubsets)):
                 if i == curr :
                     continue
                 else :
                     (w0,weights)=updateWeights(listOfSubsets[i], w0, weights)
-#             for ind in range(len(listOfSubsets[curr])) :
-#                 if listOfSubsets[ind].attrValues[-1] == predictions[ind] :
-#                     correctOutputs += 1
 
     predictions = makePredictions(trainingInstances,w0,weights)
     correctOutputs = 0 
@@ -180,7 +153,6 @@ def main():
             correctOutputs+=1
     print "Total correct =",correctOutputs,"out of",len(trainingInstances)
     
-#     print '\n\n\n',w0,weights
 
 main()
 
